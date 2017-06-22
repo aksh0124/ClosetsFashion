3e3876b30e1e03d3de4afa8b99ed8d20ed3d1a1c
aksh0124 <aksh271@gmail.com>
2017-06-22 12:37:58 +0530

    Add files via upload

---
+++ ClosetsFashion/ClosetsFashion/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ClosetsFashion
+//
+//  Created by asc6 on 6/21/17.
+//  Copyright © 2017 asc6. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
